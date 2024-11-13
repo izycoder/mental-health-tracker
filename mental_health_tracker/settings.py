fb347ce3563c7549bd09eca90f17d5a55efdd396
izycoder <farizmuhammad39@gmail.com>
2024-11-13 14:48:31 +0700

    tugas 8 done

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'authentication',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -51,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'mental_health_tracker.urls'
@@ -131,4 +134,12 @@ else:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://fariz-muhammad31-mentalhealthtracker2.pbp.cs.ui.ac.id", "https://fariz-muhammad31-mentalhealthtracker2.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://fariz-muhammad31-mentalhealthtracker2.pbp.cs.ui.ac.id", "https://fariz-muhammad31-mentalhealthtracker2.pbp.cs.ui.ac.id", "http://10.0.2.2"]
+
+# CORS and Security Settings
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
